7c5dfaf583a1a7da359675b0a82a5c826ba36881
Ramiz Sabovic <frostywinterr@gmail.com>
2022-11-06 12:49:17 +0100

    Create Basic App Router

--- clkwrk/src/index.js
+++ clkwrk/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppRouter from './components/AppRouter';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <AppRouter/>
   </React.StrictMode>
 );
 
